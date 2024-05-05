1020dde8270b20fab0aa383f003984897d723d6d
Željko Beratovic <65452618+zeljkoberatovic@users.noreply.github.com>
2024-05-05 09:38:06 +0200

    loading attendee relations

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -4,34 +4,41 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AttendeeResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Attendee;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
 {
+
+    use CanLoadRelationships;
+
+    private array $relations = ['user'];
+
     /**
      * Display a listing of the resource.
      */
     public function index(Event $event)
     {
-        $attendees = $event->attendees()->latest();
         
-        return AttendeeResource::collection(
-            $attendees->paginate()
+        $attendee = $this->loadRelationships(
+            $event->attendees()->create([
+                'user_id' => 1
+            ])
         );
+
+        return new AttendeeResource($attendee);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request, Event $event)
+    public function store(Request $request, Event $event, Attendee $attendee)
     {
-        $attendee = $event->attendees()->create([
-            'user_id' => 1
-        ]);
-
-        return new AttendeeResource($attendee);
+        return new AttendeeResource(
+            $this->loadRelationships($attendee)
+        );
     }
 
     /**
